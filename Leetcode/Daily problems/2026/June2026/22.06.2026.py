188ff5061cbb51d83188f35d7e093423735d1081
DHILEEPAN T <dhileepantv@gmail.com>
2026-06-22 11:31:51 +0530

    daily question

---
+++ Leetcode/Daily problems/2026/June2026/22.06.2026.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = {'b' : 0, 'a' : 0, 'l' : 0, 'o' : 0, 'n' : 0}
+        for i in text:
+            if i in 'ballon':
+                d[i] += 1
+        return min(d['b'], d['a'], d['n'], d['l']//2, d['o']//2)

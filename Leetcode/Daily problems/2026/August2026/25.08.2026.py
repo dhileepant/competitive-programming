9ef7632003115b6ed8d448dddb7e36e2502c93f3
DHILEEPAN T <dhileepantv@gmail.com>
2026-08-25 11:36:00 +0530

    daily question

---
+++ Leetcode/Daily problems/2026/August2026/25.08.2026.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        s = set(nums) 
+        for i in range(1, 102):
+            if i*k not in s:
+                return i*k
